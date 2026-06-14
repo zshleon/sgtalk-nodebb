7d5d6adf2461ef4e46a3ff19bb023b92cba585e7
HUI <zshstc@gmail.com>
2026-06-14 15:47:04 +0000

    Reskin: warm-editorial design system (round 4 phase 1)
    - New scss/sgtalk-ds.scss: override design tokens to warm 'C' palette (light + warm-night dark)
    - Serif (Noto Serif SC) signature on titles, brand wordmark, box titles
    - library.js: load Noto Serif SC alongside Inter
    - imported last in theme.scss

--- themes/nodebb-theme-sgtalk-v2ex/library.js
+++ themes/nodebb-theme-sgtalk-v2ex/library.js
@@ -88,7 +88,7 @@ library.addFontLinks = async function (data) {
 	data.links.push(
 		{ rel: 'preconnect', href: 'https://fonts.googleapis.com' },
 		{ rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
-		{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap' }
+		{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Noto+Serif+SC:wght@500;600;700&display=swap' }
 	);
 	return data;
 };
